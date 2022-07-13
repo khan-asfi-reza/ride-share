c3c693cc2af9e8371bdcfcef68d277dd149321a4
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-07-13 21:37:26 +0600

    :sparkles: Mapbox addition

--- src/screens/Map.tsx
+++ src/screens/Map.tsx
@@ -1,10 +1,16 @@
-import {Text} from "react-native";
+import {Text, View} from "react-native";
+// @ts-ignore
+import {MAPBOX} from "@env";
+import tw from "@lib/tailwind";
+import MapboxGL from '@rnmapbox/maps';
+
+MapboxGL.setAccessToken(MAPBOX);
 
 function Map() {
     return (
-        <Text>
-            Hello World
-        </Text>
+        <View style={tw`justify-center align-items-center flex-1`}>
+            <MapboxGL.MapView style={{flex: 1}} />
+        </View>
         );
 }
 
