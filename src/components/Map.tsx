c33024ee6cdb90905270bbe40f56e9035d7eac9f
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-07-14 02:19:45 +0600

    :sparkles: Update App

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,28 +2,19 @@ import MapView, {Marker} from "react-native-maps";
 import tw from "@lib/tailwind";
 import {useSelector} from "react-redux";
 import {selectOrigin} from "@slices/navSlice";
+import MapboxGL from "@rnmapbox/maps";
+// @ts-ignore
+import {MAPBOX} from "@env";
+
+MapboxGL.setAccessToken(MAPBOX)
 
 function Map() {
     const origin = useSelector(selectOrigin);
 
     return (
-       <MapView
-           style={tw`flex-1`}
-           initialRegion={{
-               latitude: 37.78825,
-               longitude: -122.4324,
-               latitudeDelta: 0.0922,
-               longitudeDelta: 0.0421,
-       }}>
-           <Marker
-               title={"Origin"}
-               description={"Description"}
-               identifier={"origin"}
-               coordinate={{
-               latitude: 37.78825,
-               longitude: -122.4324,
-           }}/>
-       </MapView>
+       <MapboxGL.MapView style={tw`flex-1`}>
+
+       </MapboxGL.MapView>
     );
 }
 
