b702ab3169fc98bc39a3c7713b64655c10a70180
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-07-15 16:23:19 +0600

    :sparkles: Ride options update

---
+++ src/components/RideOptions.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function RideOptions() {
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+export default RideOptions;
