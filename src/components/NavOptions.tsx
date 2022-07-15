d9864c9c97febc812e068343771a3736549134af
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-07-15 16:21:17 +0600

    :sparkles: Nav options new elements added

--- src/components/NavOptions.tsx
+++ src/components/NavOptions.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {FlatList, Image, Text, TouchableOpacity, View} from "react-native";
 import car from "@assets/car.png";
-import food from "@assets/burger.png";
+import chopper from "@assets/helicopter.png";
 import tw from "@lib/tailwind";
 import Icon from 'react-native-vector-icons/AntDesign';
 import {useNavigation} from "@react-navigation/native";
@@ -16,24 +16,29 @@ const data = [
     },
     {
         id: "2",
-        title: "Order Food",
-        image: food,
+        title: "Get a Chopper",
+        image: chopper,
         screen: "Map",
     }
 ]
 
-function NavOptions() {
+function NavOptions({query}) {
 
     const navigation = useNavigation();
 
     return (
         <FlatList data={data} horizontal style={tw`w-full`} scrollEnabled={false} keyExtractor={(item) => item.id} renderItem={({item}) => (
             <TouchableOpacity
+                disabled={!query}
                 onPress={()=>{
                     // @ts-ignore
                     navigation.navigate(item.screen)
                 }}
-                style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-36 rounded-md`}>
+                style={tw.style(`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-36 rounded-md`,
+                !query ? tw`opacity-20` : tw`opacity-100`
+                )
+
+            }>
                 <View>
                     <Image source={item.image} style={{height: 80, width: 80, resizeMode: 'contain'}}/>
                     <Text style={tw`text-medium mt-4`}>
