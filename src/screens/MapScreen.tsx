9c6662c8ecf14eeece656ea27414656f99dcf7f8
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-07-15 14:22:54 +0600

    :sparkles: Map Screen Update

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,15 +1,26 @@
-import {Text, View} from "react-native";
+import {View} from "react-native";
 import tw from "@lib/tailwind";
 import Map from "@components/Map";
-
+import {createStackNavigator} from "@react-navigation/stack";
+import NavigateCard from "@components/NavigateCard";
+import RideOptionsCard from "@components/RideOptions"
 function MapScreen() {
+    const Stack = createStackNavigator();
+
     return (
         <View>
             <View style={tw`h-1/2`}>
                 <Map/>
             </View>
             <View style={tw`h-1/2`}>
-
+                <Stack.Navigator>
+                    <Stack.Screen name={"NavigateCard"}
+                                  component={NavigateCard}
+                                  options={{headerShown: false}}/>
+                    <Stack.Screen name={"RideOptionsCard"}
+                                  component={RideOptionsCard}
+                                  options={{headerShown: false}}/>
+                </Stack.Navigator>
             </View>
         </View>
     );
