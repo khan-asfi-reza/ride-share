1bc5fb9c165576285607018ad147f6648315a5b2
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-07-17 18:00:38 +0600

    :sparkles: Navigation Card

--- src/components/NavigateCard.tsx
+++ src/components/NavigateCard.tsx
@@ -1,9 +1,13 @@
 import React, {useState} from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
 import tw from "@lib/tailwind";
 import MapAutocomplete from "@components/MapAutocomplete";
 import {useDispatch} from "react-redux";
 import {setDestination} from "@slices/navSlice";
+import Shortcuts from "@components/Shortcuts";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import {useNavigation} from "@react-navigation/native";
+import RideOptions from "@components/RideOptions";
 
 function NavigateCard() {
 
@@ -15,6 +19,8 @@ function NavigateCard() {
             location: data.geometry.coordinates
         }))
     }
+
+    const navigate = useNavigation();
 
     const resetDestination = () => {
         dispatch(setDestination(null))
@@ -31,6 +37,26 @@ function NavigateCard() {
                                  query={query}
                                  setQuery={setQuery}
                                  onLocationClick={onLocationClick}/>
+                <Shortcuts/>
+            </View>
+            <View style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-200`}>
+                <TouchableOpacity
+                onPress={()=>{
+                    // @ts-ignore
+                    navigate.navigate('RideOptionsCard')
+                }}
+                style={tw`flex flex-row justify-between bg-black w-24 px-4 py-2 rounded-full`}
+                >
+                    <Icon name={"car"} size={16} color={"white"}/>
+                    <Text style={tw`text-white text-center text-regular`}>Rides</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={tw`flex flex-row justify-between w-24 px-4 py-2 rounded-full`}
+                >
+                    <Icon name={"helicopter"} size={16} color={"black"}/>
+                    <Text style={tw`text-center text-regular`}>Heli</Text>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     );
