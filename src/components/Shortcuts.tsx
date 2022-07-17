0c5919461ce13ca621c742cbbc30ef2bd35f0123
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-07-18 01:08:21 +0600

    :sparkles: Shortcut Functionality

--- src/components/Shortcuts.tsx
+++ src/components/Shortcuts.tsx
@@ -2,35 +2,82 @@ import {FlatList, Text, TouchableOpacity, View} from "react-native";
 import tw from "@lib/tailwind";
 import Icon from "react-native-vector-icons/Ionicons";
 import {useNavigation} from "@react-navigation/native";
+import {useEffect, useState} from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {StackNavigationProp} from "@react-navigation/stack";
 
 const favorites = [
     {
         id: '1',
         icon: "home",
         location: "Home",
-        destination: "Palo Alto, San Francisco, USA"
+        destination: "Set Location",
+        coordinates: null,
+        setup: false
     },
     {
         id: '2',
         icon: "briefcase",
         location: "Work",
-        destination: "Palo Alto, San Francisco, USA"
+        destination: "Set Location",
+        coordinates: null,
+        setup: false
     },
 ]
 
+export interface ShortcutProps{
+    rerender?: boolean,
+    dispatcher?(coordinates): void
+}
 
-export default function Shortcuts(){
-    const navigation = useNavigation();
+export default function Shortcuts({rerender, dispatcher}: ShortcutProps){
+    const navigation = useNavigation<StackNavigationProp<any>>();
+    const [shortcut, setShortcut] = useState([]);
+
+    const getShortcutFromStorage = async (key, replacement) => {
+        const shortcut = await AsyncStorage.getItem(key);
+        return shortcut || replacement
+    }
+
+    const getShortcuts = async () => {
+        try {
+            const shortcuts = [];
+            const home = await getShortcutFromStorage('Home', favorites[0]);
+            const office = await getShortcutFromStorage('Work', favorites[1]);
+            shortcuts.push(JSON.parse(home), JSON.parse(office));
+            setShortcut(shortcuts);
+        }
+        catch (e) {
+            setShortcut(favorites)
+        }
+    }
+
+    useEffect(()=>{
+        getShortcuts().then()
+    }, [rerender])
 
     return(
         <FlatList style={tw`mt-4`}
-                  data={favorites}
+                  data={shortcut}
                   keyExtractor={(item)=>item.id}
                   ItemSeparatorComponent={()=>(
                       <View style={tw`h-1 border-t border-gray-200`}/>
                   )}
                   renderItem={({item})=>(
-            <TouchableOpacity style={tw`flex-row items-center p-3`}>
+            <TouchableOpacity
+                onPress={()=>{
+                    if(!item.setup){
+                        navigation.navigate('SetShortcutScreen', {
+                            action: item.location,
+                            shortcut: item,
+                        })
+                    }else{
+                        if(dispatcher){
+                            dispatcher(item.coordinates)
+                        }
+                    }
+                }}
+                style={tw`flex-row items-center p-3`}>
                 <Icon name={item.icon} style={tw`mr-4 rounded-full bg-gray-300 p-3`} color={"white"} size={17}/>
                 <View>
                     <Text style={tw`text-regular text-lg text-gray-600`}>
