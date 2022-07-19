0bdd8cad1ab8d4e164c40f888d6c823d02248462
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-07-19 22:49:43 +0600

    :art: Contains Profile Stack Components

--- src/stacks/ProfileStack.tsx
+++ src/stacks/ProfileStack.tsx
@@ -1,25 +1,18 @@
 import {SafeAreaView, Text, View} from "react-native";
 import {createStackNavigator} from "@react-navigation/stack";
 import tw from "@lib/tailwind";
+import Shortcuts from "@components/Shortcuts";
+import SetShortcutScreen from "@screens/SetShortcutScreen";
+import ProfileScreen from "@screens/ProfileScreen";
 
 const Stack = createStackNavigator();
 
-const Profile = () => (
-    <SafeAreaView style={tw`p-2`}>
-        <Text style={tw`text-4xl text-medium`}>
-            Khan Asfi Reza
-        </Text>
-        <Text style={tw`my-2 p-2 text-center rounded-full bg-gray-200 w-15`}>
-            5.0 ⭐
-        </Text>
 
-    </SafeAreaView>
-)
 const ProfileStack = ()=> (
     <Stack.Navigator>
         <Stack.Screen name={"ProfileScreen"}
                       options={{headerShown: false}}
-                      component={Profile}
+                      component={ProfileScreen}
                       navigationKey={"profileScreen"}
         />
     </Stack.Navigator>
