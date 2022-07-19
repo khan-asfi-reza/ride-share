202b50d8597646c9a1a12a35052379ecfc109e19
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-07-19 22:47:41 +0600

    :lock: Env Variables

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,5 +1,13 @@
 declare module '@env' {
     export const GOOGLE_MAPS_API_KEY: string;
     export const MAPBOX: string;
-    export const MAPBOX_DOWNLOAD: string;
+    export const FIREBASE_API_KEY: string;
+    export const AUTH_DOMAIN: string;
+    export const PROJECT_ID: string;
+    export const STORAGE_BUCKET: string;
+    export const MESSAGING_SENDER_ID: string;
+    export const FIREBASE_APP_ID: string;
+    export const MEASUREMENT_ID: string;
+    export const DATABASE_URL: string;
+    export const GOOGLE_CLIENT_ID: string
 }
