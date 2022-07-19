77d0bc7b4081d2973a29972b220c14b057fd125c
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-07-19 22:49:55 +0600

    :art: Set Shortcut Locations

--- src/screens/SetShortcutScreen.tsx
+++ src/screens/SetShortcutScreen.tsx
@@ -1,9 +1,7 @@
 import {Image, SafeAreaView, Text, TouchableOpacity} from "react-native";
 import logo from "@assets/logoText.png";
 import MapAutocomplete from "@components/MapAutocomplete";
-import React, {useEffect, useState} from "react";
-import {useDispatch} from "react-redux";
-import {setOrigin} from "@slices/navSlice";
+import React, {useState} from "react";
 import tw from "@lib/tailwind";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {RouteProp, useRoute} from "@react-navigation/core";
@@ -13,11 +11,12 @@ import {StackNavigationProp} from "@react-navigation/stack";
 export type SetShortcutScreenParams = {
     params: {
         action: string,
-        shortcut: object
+        shortcut: object,
+        returnTo?: string
     }
 }
 
-function SetShortcutScreen() {
+function SetShortcutScreen({showHeader=true, headerComp}: {showHeader?: boolean, headerComp?:any}) {
 
     const [query, setQuery] = useState('')
     const [location, setLocation] = useState(null);
@@ -28,27 +27,35 @@ function SetShortcutScreen() {
         setLocation(data.geometry.coordinates);
     }
 
-    const { action, shortcut } = route.params;
+    const {action, shortcut, returnTo} = route.params;
 
     const onPress = async () => {
-        if(query){
+        if (query) {
             const newShortcut = {...shortcut, destination: query, coordinates: location, setup: true};
             await AsyncStorage.setItem(action, JSON.stringify(newShortcut))
-            navigation.navigate("HomeScreen", {
+            navigation.navigate(returnTo || "HomeScreen", {
                 rerender: true,
             })
         }
     }
 
 
-    return(
+    return (
         <SafeAreaView style={tw`px-4 py-2`}>
-            <Image source={logo} style={{height: 100, width: 100, resizeMode: 'contain'}}/>
+            {
+                showHeader
+                &&
+                <Image source={logo} style={{height: 100, width: 100, resizeMode: 'contain'}}/>
+            }
+            {
+                headerComp(action)
+            }
             <MapAutocomplete placeholder={action === "Home" ? "Home Location" : "Work Location"}
                              query={query}
                              setQuery={setQuery}
                              onLocationClick={onLocationClick}/>
-            <TouchableOpacity onPress={onPress} disabled={!query} style={tw.style(`bg-black mt-8 py-4 px-4`, !query && `opacity-50`)}>
+            <TouchableOpacity onPress={onPress} disabled={!query}
+                              style={tw.style(`bg-black mt-8 py-4 px-4`, !query && `opacity-50`)}>
                 <Text style={tw`text-regular text-white text-center`}>
                     {action === "Home" ? "Set Home Location" : "Set Work Location"}
                 </Text>
